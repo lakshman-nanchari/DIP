8ac4c7631f4298b364b4fff17ab1eee4b2a5df06
lakshman-nanchari <lakshmannanchari96@gmail.com>
2026-03-09 10:07:46 +0530

    frontend:feat(routes): add dashboard, datasets, upload and analytics routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,40 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import DashboardHome from "./pages/DashboardHome";
+import DatasetsPage from "./pages/DatasetsPage";
+import UploadDatasetPage from "./pages/UploadDatasetPage";
+import AnalyticsDashboardPage from "./pages/AnalyticsDashboardPage";
+
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <BrowserRouter>
+
       <Routes>
 
         <Route path="/" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
 
-        <Route path="/dashboard" element={<DashboardHome />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <DashboardHome />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route path="/datasets" element={<DatasetsPage />} />
+
+        <Route path="/upload-dataset" element={<UploadDatasetPage />} />
+
+        <Route path="/analytics" element={<AnalyticsDashboardPage />} />
 
       </Routes>
+
     </BrowserRouter>
   );
 }
