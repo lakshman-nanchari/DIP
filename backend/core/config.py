8291341d12a1b97c85d02e3a63887a3366138dbb
lakshman-nanchari <lakshmannanchari96@gmail.com>
2026-03-20 19:12:46 +0530

    backend: feat(core): add utility functions for storage and analytics support

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,9 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = os.getenv("SECRET_KEY")
 FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+# Supabase credentials
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
